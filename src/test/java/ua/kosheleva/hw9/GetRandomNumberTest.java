5730c1468e66184a0bb258b34bb7b90bc6d7aa23
Mariia Kosheleva <mrkshlv003@gmail.com>
2022-02-15 19:00:04 +0200

    add tests

--- src/test/java/ua/kosheleva/hw9/GetRandomNumberTest.java
+++ src/test/java/ua/kosheleva/hw9/GetRandomNumberTest.java
@@ -13,19 +13,45 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GetRandomNumberTest {
-    private final int num = 10;
-    private GetRandomNumber target;
+    private final Random RANDOM = Mockito.mock(Random.class, Mockito.withSettings().withoutAnnotations());
+    private GetRandomNumber target = new GetRandomNumber();
 
-    @BeforeEach
-    void setUp() {
-
-        target  = Mockito.mock(GetRandomNumber.class);
-        Mockito.when(target.get(num)).thenReturn();
+    @Test
+    void numberIs0_allFine(){
+        Mockito.when(RANDOM.nextInt(100)).thenReturn(0);
+        Assertions.assertEquals(-1, target.get(RANDOM));
     }
 
     @Test
-    void getNumber_lessThen10_isNotFine(){
-        Assertions.assertEquals(200, target.get(r));
+    void numberMoreThen0LessThen10_allFine(){
+        Mockito.when(RANDOM.nextInt(100)).thenReturn(5);
+        Assertions.assertEquals(200, target.get(RANDOM));
     }
+
+    @Test
+    void numberMoreThen10LessThen20_allFine(){
+        Mockito.when(RANDOM.nextInt(100)).thenReturn(15);
+        Assertions.assertEquals(3, target.get(RANDOM));
+    }
+
+    @Test
+    void numberMoreThen30LessThen50_allFine(){
+        Mockito.when(RANDOM.nextInt(100)).thenReturn(30);
+        Assertions.assertEquals(null, target.get(RANDOM));
+    }
+
+    @Test
+    void numberMoreThen50LessThen80_allFine(){
+        Mockito.when(RANDOM.nextInt(100)).thenReturn(70);
+        Assertions.assertEquals(-10, target.get(RANDOM));
+    }
+
+    @Test
+    void numberMoreThen80LessThen100_allFine(){
+        Mockito.when(RANDOM.nextInt(100)).thenReturn(90);
+        Assertions.assertEquals(90, target.get(RANDOM));
+    }
+
+
 
 }
